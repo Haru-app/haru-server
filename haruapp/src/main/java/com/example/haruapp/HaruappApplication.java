3144048c13e6140c9e1079ead149b474df62b520
imyxxnsj <imyxxnsj@gmail.com>
2025-07-23 10:38:39 +0900

    프로젝트 초기 디렉토리 구조 및 .gitkeep 추가

---
+++ haruapp/src/main/java/com/example/haruapp/HaruappApplication.java
@@ -0,0 +1,13 @@
+package com.example.haruapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruappApplication.class, args);
+	}
+
+}
---
+++ haruapp/src/test/java/com/example/haruapp/HaruappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haruapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaruappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
