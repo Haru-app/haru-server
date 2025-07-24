ba48c65150ff4f1fc721ff8149d21900a2bbd7aa
junhyeonpark <j0303p@gmail.com>
2025-07-24 13:07:12 +0900

    feat: 날씨 API 통신 및 결과 반환 서비스

---
+++ src/main/java/com/example/haruapp/weather/service/WeatherApiClient.java
@@ -0,0 +1,56 @@
+package com.example.haruapp.weather.service;
+
+import com.example.haruapp.global.error.CustomException;
+import com.example.haruapp.global.error.ErrorCode;
+import com.example.haruapp.weather.dto.WeatherForecastDto;
+import com.example.haruapp.weather.util.DateFormatUtil;
+import com.example.haruapp.weather.util.WeatherXMLCleaningUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.client.RestTemplate;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import java.net.URI;
+
+@Slf4j
+@RequiredArgsConstructor
+public class WeatherApiClient {
+
+    private final RestTemplate restTemplate;
+    private final String serviceKey;
+    private final AIService aiService;
+
+    public String getUltraShortForecast() {
+
+        String weatherResultXML = getWeatherResultXML();
+
+        WeatherForecastDto weatherForecastDto= parseForecastToDto(weatherResultXML);
+        log.info("날씨 DTO: {}", weatherForecastDto);
+
+        String weatherSummary =aiService.generation(weatherForecastDto);
+        log.info("날씨 요약: {}", weatherSummary);
+        return weatherSummary;
+    }
+
+    public String getWeatherResultXML(){
+        String baseDate = DateFormatUtil.getBaseDate();  // "yyyyMMdd"
+        String baseTime = DateFormatUtil.getBaseTime();  // "HHmm"
+        String url = String.format(WeatherXMLCleaningUtil.BASE_URL, serviceKey, baseDate, baseTime, WeatherXMLCleaningUtil.NX, WeatherXMLCleaningUtil.NY);
+
+        try {
+            return restTemplate.getForObject(URI.create(url), String.class);
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.WEATHERAPI_SERVER_ERROR);
+        }
+    }
+
+    public WeatherForecastDto parseForecastToDto(String xml) {
+
+        Document doc = WeatherXMLCleaningUtil.parseXml(xml);
+        doc.getDocumentElement().normalize();
+
+        NodeList itemList = doc.getElementsByTagName("item");
+        return WeatherXMLCleaningUtil.dataCleaning(itemList);
+    }
+}
