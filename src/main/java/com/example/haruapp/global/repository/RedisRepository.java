695c0691c16c3c4fe4d528856a040773a9634a4f
junhyeonpark <j0303p@gmail.com>
2025-07-24 13:41:27 +0900

    feat: redis 날짜와 시간을 통해 데이터 캐싱

---
+++ src/main/java/com/example/haruapp/global/repository/RedisRepository.java
@@ -0,0 +1,24 @@
+package com.example.haruapp.global.repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RedisRepository {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public String getWeather(String baseDate, String baseTime){
+        return redisTemplate.opsForValue().get(baseDate + baseTime);
+    }
+
+    public void setWeather(String baseDate,String baseTime,String weather) {
+        redisTemplate.opsForValue().set(baseDate+baseTime, weather, Duration.ofMinutes(30));
+    }
+}
