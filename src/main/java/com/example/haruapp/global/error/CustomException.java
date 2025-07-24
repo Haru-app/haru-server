07813fb53d38e650dfacee3e281da4d61feb69d6
junhyeonpark <j0303p@gmail.com>
2025-07-24 11:23:56 +0900

    feat: 커스텀 에러 처리 추가

---
+++ src/main/java/com/example/haruapp/global/error/CustomException.java
@@ -0,0 +1,15 @@
+package com.example.haruapp.global.error;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    @Override
+    public String getMessage() {
+        return errorCode.getMessage();
+    }
+}
---
+++ src/main/java/com/example/haruapp/global/error/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.example.haruapp.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public enum ErrorCode {
+
+
+    SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다. 서버 팀에 연락주세요!"),
+    INVALID_INDEX(HttpStatus.BAD_REQUEST, "잘못된 인덱스입니다."),
+    INVALID_INPUT(HttpStatus.BAD_REQUEST, "잘못된 입력입니다."),
+    JSON_CONVERT_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "JSON 파싱 실패" ),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 서버 오류입니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN,"권한이 부족합니다." ),
+
+            ;
+
+    // Member
+    private final HttpStatus status;
+    private final String message;
+}
