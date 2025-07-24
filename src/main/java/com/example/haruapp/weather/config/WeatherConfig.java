19c4e1d0590af36d9e709a1dc6a2a1fe77cc46e1
junhyeonpark <j0303p@gmail.com>
2025-07-24 13:04:25 +0900

    feat: 날씨서비스에 api 주입 config

---
+++ src/main/java/com/example/haruapp/weather/config/WeatherConfig.java
@@ -0,0 +1,22 @@
+package com.example.haruapp.weather.config;
+
+import com.example.haruapp.weather.service.AIService;
+import com.example.haruapp.weather.service.WeatherApiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class WeatherConfig {
+
+    @Value("${weather.key}")
+    private String weatherKey;
+
+    @Bean
+    public WeatherApiClient weatherApiClient(AIService aiService) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        return new WeatherApiClient(restTemplate, weatherKey,aiService);
+    }
+}
