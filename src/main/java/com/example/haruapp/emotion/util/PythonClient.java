aa982505d37f27516c84f730c4537a4d3c3f2340
junhyeonpark <j0303p@gmail.com>
2025-07-25 14:36:42 +0900

    fix: 파이썬 서버 원격으로 수정

--- src/main/java/com/example/haruapp/emotion/util/PythonClient.java
+++ src/main/java/com/example/haruapp/emotion/util/PythonClient.java
@@ -10,8 +10,8 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class PythonClient {
 
-//    public static final String PYTHON_API_URL = "http://원격 주소:8000/question/similarity-map";
-    public static final String PYTHON_API_URL = "http://localhost:8000/question/similarity-map";
+    public static final String PYTHON_API_URL = "http://3.35.210.83:8000/question/similarity-map";
+//    public static final String PYTHON_API_URL = "http://localhost:8000/question/similarity-map";
     private static final RestTemplate restTemplate = new RestTemplate();
 
     public static void callingPythonServer() {
@@ -24,6 +24,7 @@ public class PythonClient {
             }
             log.info("✅ Python 서버 응답 확인됨 (200 OK)");
         } catch (Exception e) {
+            log.error("❗ Python 서버 요청 중 예외 발생", e);
             throw new CustomException(ErrorCode.SIMILARITY_FETCH_FAILED);
         }
     }
