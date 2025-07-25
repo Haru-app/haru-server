e73207dd5df9bf6afc28ed6e55102e81446d66b2
junhyeonpark <j0303p@gmail.com>
2025-07-25 14:00:31 +0900

    feat: redis에 저장된 유사도 없을시 python에 생성 요청

---
+++ src/main/java/com/example/haruapp/emotion/util/PythonClient.java
@@ -0,0 +1,30 @@
+package com.example.haruapp.emotion.util;
+
+import com.example.haruapp.global.error.CustomException;
+import com.example.haruapp.global.error.ErrorCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+@Slf4j
+public class PythonClient {
+
+//    public static final String PYTHON_API_URL = "http://원격 주소:8000/question/similarity-map";
+    public static final String PYTHON_API_URL = "http://localhost:8000/question/similarity-map";
+    private static final RestTemplate restTemplate = new RestTemplate();
+
+    public static void callingPythonServer() {
+        try {
+
+            ResponseEntity<String> response = restTemplate.getForEntity(PYTHON_API_URL, String.class);
+
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new CustomException(ErrorCode.SIMILARITY_FETCH_FAILED);
+            }
+            log.info("✅ Python 서버 응답 확인됨 (200 OK)");
+        } catch (Exception e) {
+            throw new CustomException(ErrorCode.SIMILARITY_FETCH_FAILED);
+        }
+    }
+}
