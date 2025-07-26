b29bda8217b445ce804d458af3d99ae1c81ca893
junhyeonpark <j0303p@gmail.com>
2025-07-27 00:20:59 +0900

    fix: 감정 전송시 색상, 설명 추가

--- src/main/java/com/example/haruapp/emotion/dto/EmotionResponse.java
+++ src/main/java/com/example/haruapp/emotion/dto/EmotionResponse.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class EmotionResponse {
     private Long emotionId;
     private String emotionName;
+    private String emotionColor;
+    private String emotionDescription;
 }
