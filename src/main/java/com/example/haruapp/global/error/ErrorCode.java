0091b5c94c22d8f23280cc0ca1d4922c00e29b6d
jhy <65846779+jhy11@users.noreply.github.com>
2025-07-26 23:46:43 +0900

    feat: 코스 저장, 수정, 스크랩 api 추가

--- src/main/java/com/example/haruapp/global/error/ErrorCode.java
+++ src/main/java/com/example/haruapp/global/error/ErrorCode.java
@@ -16,6 +16,11 @@ public enum ErrorCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "예기치 못한 서버 오류입니다."),
     ACCESS_DENIED(HttpStatus.FORBIDDEN,"권한이 부족합니다." ),
 
+    // 코스
+    COURSE_STORE_COUNT_INVALID(HttpStatus.BAD_REQUEST, "코스별 매장 개수는 6개여야 합니다."),
+    COURSE_UPDATE_UNAUTHORIZED(HttpStatus.FORBIDDEN, "본인 코스만 수정할 수 있습니다."),
+    COURSE_NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 코스가 존재하지 않습니다."),
+    COURSE_SCRAP_SELF(HttpStatus.BAD_REQUEST, "본인 코스는 스크랩할 수 없습니다.")
             ;
 
     // Member
