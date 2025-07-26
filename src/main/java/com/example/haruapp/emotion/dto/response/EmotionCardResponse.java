d7a4835d3d12a91f636ae08dfc0394ed13d524d7
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-07-26 18:13:55 +0900

    feat: 감정 카드 생성 API dto 작성

---
+++ src/main/java/com/example/haruapp/emotion/dto/response/EmotionCardResponse.java
@@ -0,0 +1,13 @@
+package com.example.haruapp.emotion.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class EmotionCardResponse {
+
+	private boolean status;
+
+	private String message;
+}
