eb24a792a93c3fe0731ef18534567780a6fd62fa
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-07-26 18:00:36 +0900

    feat: google cloud storage 접근 위한 util 코드 구현

--- src/main/java/com/example/haruapp/util/GcpStorageUtil.java
+++ src/main/java/com/example/haruapp/util/GcpStorageUtil.java
@@ -1,6 +1,15 @@
 package com.example.haruapp.util;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +17,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class GcpStorageUtil {
 
+	private final Storage storage = StorageOptions.getDefaultInstance().getService();
+
+	private final String bucketName = "haru_image";
+
+	public String upload(MultipartFile file) {
+
+		String folder = "emotion/" + LocalDate.now();
+		String originalName = file.getOriginalFilename();
+		String extension = originalName.substring(originalName.lastIndexOf("."));
+		String filename = UUID.randomUUID() + extension;
+		String blobName = folder + "/" + filename;
+
+		try {
+			BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, blobName)
+				.setContentType(file.getContentType())
+				.build();
+			storage.create(blobInfo, file.getBytes());
+		} catch (IOException e) {
+			throw new RuntimeException("파일 업로드 실패", e);
+		}
+
+		return String.format("https://storage.googleapis.com/%s/%s", bucketName, blobName);
+	}
 }
