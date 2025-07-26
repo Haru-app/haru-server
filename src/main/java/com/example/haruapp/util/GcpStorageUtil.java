0c8afa480fd30479622c534c934311dccbc3aa1d
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-07-26 17:56:42 +0900

    add: 구글 클라우드 스토리지 접근위해 의존성 추가

---
+++ src/main/java/com/example/haruapp/util/GcpStorageUtil.java
@@ -0,0 +1,11 @@
+package com.example.haruapp.util;
+
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class GcpStorageUtil {
+
+}
