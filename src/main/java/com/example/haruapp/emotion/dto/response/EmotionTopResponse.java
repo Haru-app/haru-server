3dd8fdbb4bfd56074820519271df6be6e42cf504
junhyeonpark <j0303p@gmail.com>
2025-07-28 11:55:45 +0900

    fix: 발환시 dto를 사용

---
+++ src/main/java/com/example/haruapp/emotion/dto/response/EmotionTopResponse.java
@@ -0,0 +1,10 @@
+package com.example.haruapp.emotion.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EmotionTopResponse {
+    private String emotion;
+}
